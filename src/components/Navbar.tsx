49c86714ea65b39e2eaeb826142a285d9ba7abe6
Eemil.Korkka <eemil.korkka@gmail.com>
2025-02-05 13:48:09 +0200

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav className="navbar">
+                <h1>Where in the world?</h1>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
+  
